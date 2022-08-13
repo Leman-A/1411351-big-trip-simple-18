0cba880c6510b98cf215b294409ea6e8b9145040
Leman <niklaysm@yandex.ru>
2022-08-13 17:08:09 +0300

    правки

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import FiltersView from './view/filters-view.js';
 import {render} from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 
-const siteMainElement = document.querySelector('.page-body__page-main');
+const tripEventsSection = document.querySelector('.trip-events');
 const tripControlsFiltres = document.querySelector('.trip-controls__filters');
 const boardPresenter = new BoardPresenter();
 
 
 render(new FiltersView(), tripControlsFiltres);
-boardPresenter.init(siteMainElement);
+boardPresenter.init(tripEventsSection);
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,12 +1,9 @@
-// import NewPointView from '../view/new-point-view.js';
+import {render} from '../render.js';
 import PointsView from '../view/point-view.js';
 import SortView from '../view/sort-view.js';
-import {render} from '../render.js';
 import TripListView from '../view/trip-events-list-view';
 import NewPointView from '../view/new-point-view.js';
 import EditPointView from '../view/edit-point-view.js';
-
-const tripEvents = document.querySelector('.trip-events');
 
 export default class BoardPresenter {
   eventsList = new TripListView();
@@ -14,8 +11,8 @@ export default class BoardPresenter {
   init = (boardContainer) => {
     this.boardContainer = boardContainer;
 
-    render(new SortView(), tripEvents);
-    render(this.eventsList, tripEvents);
+    render(new SortView(), this.boardContainer);
+    render(this.eventsList, this.boardContainer);
     render(new EditPointView, this.eventsList.getElement());
     render(new NewPointView(), this.eventsList.getElement());
 
